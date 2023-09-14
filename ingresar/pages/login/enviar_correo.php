2459a30279084e222ce07a19b51bf081e54a699f
JennyferSanchez <105504708+JennyferSanchez@users.noreply.github.com>
2023-09-14 12:30:40 -0500

    Update enviar_correo.php

--- ingresar/pages/login/enviar_correo.php
+++ ingresar/pages/login/enviar_correo.php
@@ -2,7 +2,7 @@
 function sendVerificationCode($to, $code) {
     $subject = "Código de verificación";
     $message = "Tu código de verificación es: $code";
-    $headers = "From: EcoMatrixs.a.s@gmail.com"; // Cambia esto
+    $headers = "From: ecomtrix@jennycs.site"; // Cambia esto
     return mail($to, $subject, $message, $headers);
 }
 ?>
