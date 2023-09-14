08ab6a5fd03ff04dd9c1c328e6d59d7266a7a46d
jennyfer S <jennycita.k09@gmail.com>
2023-09-14 12:57:19 -0500

    prueba

--- ingresar/pages/login/enviar_correo.php
+++ ingresar/pages/login/enviar_correo.php
@@ -2,7 +2,7 @@
 function sendVerificationCode($to, $code) {
     $subject = "Código de verificación";
     $message = "Tu código de verificación es: $code";
-    $headers = "From: ecomtrix@jennycs.site"; // Cambia esto
+    $headers = "From: ecomatrix@jennycs.site"; // Cambia esto
     return mail($to, $subject, $message, $headers);
 }
 ?>
