0a9c1436f639dff0771a5fcc2494f3bfe0c99d5c
jennyfer S <jennycita.k09@gmail.com>
2023-09-14 13:44:27 -0500

    prueba2

--- ingresar/pages/login/register.php
+++ ingresar/pages/login/register.php
@@ -8,14 +8,17 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $username =  $_POST["username"];
   $password = password_hash($_POST["password"], PASSWORD_BCRYPT);
   $email = $_POST["email"];
-  if($username== "" && $password== "" && $email== ""){
+  if($username!= " " && $password!= " " && $email!= " "){
     // Generar un código de verificación aleatorio
     $verificationCode = mt_rand(100000, 999999);
 
     $query1 = "SELECT email FROM usuarios WHERE email = '$email'";
     $result1 = mysqli_query($conn, $query1);
 
-    if ($result->num_rows < 0){
+    while ($row = mysqli_fetch_assoc($result1)) {
+      echo $row['email'];
+    }
+    if ($result1->num_rows == 0){
       $query = "INSERT INTO usuarios ( username, password, email, verified, verification_code) VALUES ('$username', '$password', '$email', 0,   '$verificationCode')";
       $result = mysqli_query($conn, $query);
 
@@ -38,6 +41,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }else{
       $error = "Correo ya registrado resgitra otro.";
     }
+  }else{
+    $error = "llena los campos.";
   }
 }
 
