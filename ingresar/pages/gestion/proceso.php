f3c9130a496c42b97ab53ebb89dc0142109ab53e
jennyfer S <jennycita.k09@gmail.com>
2023-09-19 12:09:37 -0500

    19/09/20231

--- ingresar/pages/gestion/proceso.php
+++ ingresar/pages/gestion/proceso.php
@@ -39,6 +39,7 @@ $result = mysqli_query($conn, $query);
   <link rel="stylesheet" href="../../css/vertical-layout-light/style.css">
   <link rel="stylesheet" href="../../css/skilline.css">
   <link rel="shortcut icon" href="../../../img/logo_eco.ico" />
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
 
 </head>
 
